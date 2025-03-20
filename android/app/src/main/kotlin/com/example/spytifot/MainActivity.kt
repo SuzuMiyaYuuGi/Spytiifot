b6bc4391ce1363fa7699008b8dd1f8fbce8a9470
Cruzz-name <lupangch007@gmail.com>
2025-03-20 19:31:04 +0700

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/spytifot/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.spytifot
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Spytifot',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('หน้าแรก'),
+      ),
+      body: Center(
+        child: Text('ยินดีต้อนรับสู่ Spytifot!'),
+      ),
+    );
+  }
+}
