d249f1d53e4c997e08551868a2f5d12c1428ce7d
sittitanaporn-khamlom <sittitanaporn@gmail.com>
2025-03-20 23:29:25 +0700

    Update bottom_nav_bar.dart

--- lib/widgets/bottom_nav_bar.dart
+++ lib/widgets/bottom_nav_bar.dart
@@ -2,33 +2,53 @@ import 'package:flutter/material.dart';
 import '../routes.dart';
 
 class BottomNavBar extends StatelessWidget {
+  final int currentIndex;
+  
+  BottomNavBar({required this.currentIndex});
+
+  void _onItemTapped(BuildContext context, int index) {
+    switch (index) {
+      case 0:
+        Navigator.pushNamed(context, AppRoutes.home);
+        break;
+      case 1:
+        Navigator.pushNamed(context, AppRoutes.search);
+        break;
+      case 2:
+        Navigator.pushNamed(context, AppRoutes.musicList);
+        break;
+      case 3:
+        Navigator.pushNamed(context, AppRoutes.uploadMusic);
+        break;
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return BottomNavigationBar(
       backgroundColor: Colors.black,
-      selectedItemColor: Colors.white,
-      unselectedItemColor: Colors.grey,
-      onTap: (index) {
-        switch (index) {
-          case 0:
-            Navigator.pushNamed(context, AppRoutes.home);
-            break;
-          case 1:
-            Navigator.pushNamed(context, AppRoutes.musicList);
-            break;
-          case 2:
-            Navigator.pushNamed(context, AppRoutes.musicPlayer);
-            break;
-          case 3:
-            Navigator.pushNamed(context, AppRoutes.uploadMusic);
-            break;
-        }
-      },
+      selectedItemColor: Colors.greenAccent,
+      unselectedItemColor: Colors.white70,
+      currentIndex: currentIndex,
+      type: BottomNavigationBarType.fixed,
+      onTap: (index) => _onItemTapped(context, index),
       items: [
-        BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
-        BottomNavigationBarItem(icon: Icon(Icons.search), label: "Search"),
-        BottomNavigationBarItem(icon: Icon(Icons.library_music), label: "Play List"),
-        BottomNavigationBarItem(icon: Icon(Icons.upload), label: "Upload"),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.home, size: 28),
+          label: "Home",
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.search, size: 28),
+          label: "Search",
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.library_music, size: 28),
+          label: "Play List",
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.upload_file, size: 28),
+          label: "Upload",
+        ),
       ],
     );
   }
