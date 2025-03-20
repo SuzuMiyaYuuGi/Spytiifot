472ef0de00882b5689c51f02de9b7f48641ea8f8
sittitanaporn-khamlom <sittitanaporn@gmail.com>
2025-03-20 23:33:30 +0700

    Update routes.dart

--- lib/routes.dart
+++ lib/routes.dart
@@ -1,24 +1,24 @@
 import 'package:flutter/material.dart';
 import 'screens/home_screen.dart';
 import 'screens/music_list_screen.dart';
-import 'screens/music_player_screen.dart';
 import 'screens/music_detail_screen.dart';
 import 'screens/upload_music_screen.dart';
+import 'screens/search_screen.dart'; // เพิ่มไฟล์หน้าค้นหา
 
 class AppRoutes {
   static const String home = '/';
   static const String musicList = '/music-list';
-  static const String musicPlayer = '/music-player';
   static const String musicDetail = '/music-detail';
   static const String uploadMusic = '/upload-music';
+  static const String search = '/search'; // เพิ่ม route ค้นหา
 
   static Map<String, WidgetBuilder> getRoutes() {
     return {
       home: (context) => HomeScreen(),
       musicList: (context) => MusicListScreen(),
-      musicPlayer: (context) => MusicPlayerScreen(),
       musicDetail: (context) => MusicDetailScreen(),
       uploadMusic: (context) => UploadMusicScreen(),
+      search: (context) => SearchScreen(), // เพิ่มเส้นทางให้ Search
     };
   }
 }
